f71c892c8426ae1cea1cdfe95fcfad6eaa592189
ejmurc <eliasmurcray@gmail.com>
2026-03-17 01:01:25 -0700

    Update constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const API_BASE = 'http://ec2-13-59-169-35.us-east-2.compute.amazonaws.com';
+export const API_BASE = ' https://charitable-nine-gba-instantly.trycloudflare.com';
 export const WS_BASE = API_BASE.replace(/^https/, 'wss').replace(/^http/, 'ws');
